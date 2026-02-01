4519c6b44c1587dc91adec7200a0c48186babed9
arturo alvarez <arturo221355@gmail.com>
2026-02-01 16:00:53 -0600

    asdas

--- src/router/guards.ts
+++ src/router/guards.ts
@@ -1,12 +1,17 @@
 import { useAuthStore } from '@/stores/auth'
 import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router'
+import { Capacitor } from '@capacitor/core'
 
-export function authGuard(
+export async function authGuard(
   to: RouteLocationNormalized,
   from: RouteLocationNormalized,
   next: NavigationGuardNext,
 ) {
   const authStore = useAuthStore()
+
+  if (Capacitor.isNativePlatform()) {
+    await authStore.restored
+  }
 
   if (!authStore.isAuthenticated) {
     // Redirigir a la página de referencia si no está autenticado
