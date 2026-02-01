298b79af032be8afec458711e5e418a93874fc61
arturo alvarez <arturo221355@gmail.com>
2026-02-01 15:56:01 -0600

    ads

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,17 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { configureEcho } from '@laravel/echo-vue'
 import axios from 'axios'
 import { piniaCapacitorPersist } from 'pinia-plugin-capacitor-persist'
+import { Capacitor } from '@capacitor/core'
 
 const app = createApp(App)
 
 const pinia = createPinia()
 
-pinia.use(piniaPluginPersistedstate)
-pinia.use(piniaCapacitorPersist)
+if (Capacitor.isNativePlatform()) {
+  pinia.use(piniaCapacitorPersist)
+} else {
+  pinia.use(piniaPluginPersistedstate)
+}
 
 configureEcho({
   broadcaster: 'reverb',
